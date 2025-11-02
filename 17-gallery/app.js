a3565ea9307df76634da1392e504da4f7ea154d5
NORDON <georges.nordon@gmail.com>
2025-11-02 11:24:18 +0100

    réinit 17-gallery

--- 17-gallery/app.js
+++ 17-gallery/app.js
@@ -1,5 +1,3 @@
-const log = console.log;
-
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -9,6 +7,3 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
-
-//const natures = document.querySelector('.nature'); log(natures);
-log("gue se passe-t-il ?")
