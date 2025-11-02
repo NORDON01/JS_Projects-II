03cde1382d1a8c513535998e209e26b13d41934c
NORDON <georges.nordon@gmail.com>
2025-11-02 15:44:58 +0100

    setPrototypeOf

--- 17-gallery/app.js
+++ 17-gallery/app.js
@@ -7,3 +7,24 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+let geek1 = {
+    prop1() {
+        return 'Object.isExtensible()';
+    },
+    prop2() {
+        return 'JavaScript ';
+    }
+}
+let geek2 = {
+    prop3() {
+        return 'Geeksforgeeks';
+    }
+}
+
+Object.setPrototypeOf(geek2, geek1); /* geek2 reçoit les méthodes de geek1 */
+
+console.dir(geek2);
+console.log(geek2.prop3());
+console.log(geek2.prop2());
+console.log(geek2.prop1());
