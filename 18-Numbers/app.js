7e1575915a8e117cbca5e647e7510ac5faba6d93
NORDON <georges.nordon@gmail.com>
2026-01-18 17:48:24 +0100

    sauve

--- 18-Numbers/app.js
+++ 18-Numbers/app.js
@@ -1,1 +1,13 @@
-console.log('numbers project');
+const log = console.log;
+log('numbers project');
+
+const items = [...document.querySelectorAll('.number')]; log(items);
+
+const updateCount = (el) => {               //Fonction incrémentation
+    value = parseInt(el.dataset.value);     //Récupère la valeur 
+    increment = Math.ceil(value/1000);      //d'un dataset
+    //log(increment);
+    let initialValue = 0;
+}        
+
+items.forEach(item => updateCount(item));   //Utilisation de cette fonction
