9e4055918f159ad41cd87b8ffa32565ae0c91a34
NORDON <georges.nordon@gmail.com>
2026-01-18 20:14:05 +0100

    add 21-1 documentation.txt

---
+++ 21-1 json/app.js
@@ -0,0 +1,27 @@
+const log = console.log;
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {getData()});
+
+const getData = () => {
+const xhr = new XMLHttpRequest();
+log(xhr);                          //readyState 0 - not sent
+xhr.open('GET','./api/sample.txt');//readyState 1 - open
+xhr.onreadystatechange = function(){    
+    if(xhr.readyState === 4 && xhr.status === 200){
+                                   //readyState 2, 3 - loading
+        const text = document.createElement('p');
+        text.textContent = xhr.responseText;
+        document.body.appendChild(text);
+    }else{
+        log({
+            status:xhr.status,
+            text: xhr.statusText,
+            state:xhr.readyState
+        });
+    }
+}
+
+xhr.send();                        //readyState 4 - done
+}
+
+log('Hello world');
