314a197ffe2387bfa82a1344bfd0d315bba09e49
NORDON <georges.nordon@gmail.com>
2026-01-22 14:31:25 +0100

    save 21-2 fetch

--- 21-2 fetch/app.js
+++ 21-2 fetch/app.js
@@ -1,32 +1,17 @@
 const log = console.log;
-const btn = document.querySelector('.btn');
 const url = "./api/people.json";
-btn.addEventListener('click', () => {getData(url)});
-   
-function getData(url){
-const xhr = new XMLHttpRequest();
-log(xhr);                          //readyState 0 - not sent
-xhr.open('GET', url);//readyState 1 - open
-xhr.onreadystatechange = function(){    
-    if(xhr.readyState === 4 && xhr.status === 200){
-        const data = JSON.parse(xhr.responseText);
-        log(data);
-        const displayData = data.map(item => {
-            return `<p>${item.name}</p>`
-        }).join('');
-        const element = document.createElement('div');
-        element.innerHTML = displayData;
-        document.body.appendChild(element);
-    }else{
-        log({
-            status:xhr.status,
-            text: xhr.statusText,
-            state:xhr.readyState
-        });
-    }
-}
+/* const response = fetch(url);
+log(response) */
 
-xhr.send();                        //readyState 4 - done
-}
+/* fetch(url).then((resp) => {
+    log(resp);
+    return resp.json();
+}).then((data) => {
+    log(data)
+}).catch((err) => log(err)); */
 
-log('Hello world');
+fetch(url)
+.then((response) =>response.json())
+.then((data) => log(data))
+.catch((err) => log(err));
+
