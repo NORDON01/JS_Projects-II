7e325e4c61ab1a99700e4a092c03122bf6f3eb3a
nordon~ <georges.nordon@gmail.com>
2026-01-27 10:55:56 +0100

    add dad jokes

---
+++ API projects II/dad jokes/app.js
@@ -0,0 +1,32 @@
+console.log('Dad Jokes Starter');
+const log = console.log;
+const url =  'https://icanhazdadjoke.com/';
+
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+log(btn, result);
+
+btn.addEventListener('click', () => {
+    fetchDadJoke();
+})
+
+const fetchDadJoke = async() => {
+    result.textContent = 'Loading...'
+  try{
+    const response = await fetch(url, {
+        headers:{
+            Accept: 'application/json',
+            'User-Agent': 'learning app'
+        }
+    })
+    if(!response.ok){
+        throw new Error('There was an error')
+    }
+    const data = await response.json();
+    log(data);
+    result.textContent = data.joke;
+  }catch(error){
+    log(error.message);
+    result.textContent = error.message
+  };
+}; 
